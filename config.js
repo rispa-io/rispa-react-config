dbdc5e97c0b09e4af7b3af0ec364f5d2ed5b697e
dvpetrov <dvpetrov.main@gmail.com>
2018-03-02 13:54:08 +0300

    Add browser list

--- config.js
+++ config.js
@@ -7,6 +7,28 @@ const config = {
     host: 'localhost',
     port: process.env.PORT || 3000,
   },
+  browsers: [
+    // general
+    '> 1%',
+    'last 4 versions',
+
+    // desktop
+    'Explorer >= 11',
+
+    // mobile
+    'iOS >= 7.1',
+    'Android >= 4.4',
+    'UCAndroid >= 11.4',
+    'Samsung >= 4',
+
+    // exceptions
+    'not Explorer <= 10',
+    'not ExplorerMobile <= 10',
+    'not OperaMini all',
+    'not last 4 OperaMobile versions',
+    'not last 4 Baidu versions',
+    'not BlackBerry <= 9',
+  ],
 }
 
 module.exports = config
