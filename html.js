bc766f5d4ab50bd5c0d98dda4ed1bc6167396759
Dmitry Petrov <dvpetrov.main@gmail.com>
2018-12-18 21:41:21 +0300

    Remove legacy link attrs

--- html.js
+++ html.js
@@ -1,21 +1,18 @@
 const { wire } = require('viperhtml')
 
 const renderScript = path => path ? wire()`<script src="${path}" defer></script>` : ''
-const renderLink = path => path ? wire()`<link src="${path}" media="screen, projection" rel="stylesheet" type="text/css" charSet="UTF-8"/>` : ''
+const renderLink = path => path ? wire()`<link src="${path}" rel="stylesheet" type="text/css" />` : ''
 
 const renderHtml = ({
-  title,
-  meta,
   css = [],
   js = {},
 }) => wire()`
   <!doctype html>
   <html>
     <head>
-      ${[title]}
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width">
-      ${[meta]}
+      <title>RISPA project</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width" />
       ${[css.map(renderLink)]}
     </head>
     <body>
