d10f87efc0572c77e3166af65e397e5df8fe0f99
Dmitry Petrov <dvpetrov.main@gmail.com>
2018-12-30 06:13:15 +0300

    Add SSR data to html template

--- html.js
+++ html.js
@@ -1,25 +1,30 @@
 const { wire } = require('viperhtml')
+const serialize = require('serialize-javascript')
 
 const renderScript = path => path ? wire()`<script src="${path}" defer></script>` : ''
+
 const renderLink = path => path ? wire()`<link src="${path}" rel="stylesheet" type="text/css" />` : ''
 
-const renderHtml = ({
-  css = [],
-  js = {},
-}) => wire()`
+const renderInitialState = state => state ? `<script>
+  // <![CDATA[
+  window.RISPA_INITIAL_STATE=${serialize(state, { isJSON: true })};
+  // ]]>
+</script>` : ''
+
+const renderHtml = (assets, { content, state }) => wire()`
   <!doctype html>
   <html>
     <head>
       <title>RISPA project</title>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width" />
-      ${css.map(renderLink)}
+      ${assets.css.map(renderLink)}
     </head>
     <body>
-      <div id="content">
-      </div>
-      ${js.vendors.map(renderScript)}
-      ${js.chunks.map(renderScript)}
+      <div id="content">${[content]}</div>
+      ${[renderInitialState(state)]}
+      ${assets.js.vendors.map(renderScript)}
+      ${assets.js.chunks.map(renderScript)}
     </body>
   </html>
 `.toString()
